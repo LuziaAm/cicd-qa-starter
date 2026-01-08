7d10f36f8997faa139239c8a9d5de0d3b6cd5bb5
LuziaAm <luzia.castro29@gmail.com>
2026-01-08 14:05:26 -0400

    Sanity and Full tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,18 +1,23 @@
 import pytest
 from app.calculator import add, sub, mul, div
 
+@pytest.mark.sanity
 def test_add():
     assert add(2, 3) == 5
 
+@pytest.mark.sanity
 def test_sub():
     assert sub(10, 4) == 6
 
+@pytest.mark.full
 def test_mul():
     assert mul(3, 5) == 15
 
+@pytest.mark.full
 def test_div():
     assert div(10, 2) == 5
 
+@pytest.mark.full
 def test_div_by_zero():
     with pytest.raises(ValueError):
         div(10, 0)
